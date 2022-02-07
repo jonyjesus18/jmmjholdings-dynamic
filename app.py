0681386fc59ecdc40aeeffc354516ca31be4a9f3
Joao Jesus <joaomj1800@gmail.com>
2022-02-07 01:05:33 +0000

    #

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
+from datetime import date
+
+
+
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("mainPage.html")
+    today = date.today()
+    d1 = today.strftime("%B %d, %Y")
+    print(d1)
+    return render_template("mainPage.html", d1=d1)
