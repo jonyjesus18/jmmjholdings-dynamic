e116655be62f72ec3da33609446ce4bd41842c85
Joao Jesus <joaomj1800@gmail.com>
2022-02-07 21:34:07 +0000

    #

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import date
-
-
-
+import smtplib
 
 
 app = Flask(__name__)
@@ -12,4 +10,9 @@ def home():
     today = date.today()
     d1 = today.strftime("%B %d, %Y")
     print(d1)
-    return render_template("mainPage.html", d1=2)
+    return render_template("mainPage.html", d1=d1)
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    return render_template("login.html")
