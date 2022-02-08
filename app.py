6c83c24000d27df443430b0e666cbd1641f0d9cb
Joao Jesus <joaomj1800@gmail.com>
2022-02-08 00:41:06 +0000

    #

--- app.py
+++ app.py
@@ -29,21 +29,23 @@ def login():
         print(job)
         print(questions)
 
-        response = openai.Completion.create(
-        engine="text-davinci-001",
-        prompt=f"Create a list of {questions} questions for my interview as a {job}\n",
-        temperature=0.5,
-        max_tokens=150,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-)
-        text = response.choices[0].text
-        print(text)        
-
+#         response = openai.Completion.create(
+#         engine="text-davinci-001",
+#         prompt=f"Create a list of {questions} questions for my interview as a {job}\n",
+#         temperature=0.5,
+#         max_tokens=150,
+#         top_p=1,
+#         frequency_penalty=0,
+#         presence_penalty=0
+# )
+#         text = response.choices[0].text
+#         print(text)
+#         list = text.split('\n')[1:]
+#         list = filter(None, list)
+        list = ["sfsdgfsegds","sfsargsgd","sefsargsrgdsr"]
 
         return render_template("interview.html", 
-        text=text
+        list=list
         )
 
     else:
