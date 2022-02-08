93be17b21b9d66cc0c4999064ac0628fde43f74d
Joao Jesus <joaomj1800@gmail.com>
2022-02-08 00:11:41 +0000

    #

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, redirect, url_for, render_template, request
 from datetime import date
+import os
+import openai
+import json
+import requests
+openai.api_key = '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'
+
 
 app = Flask(__name__)
+
+
 
 @app.route("/")
 def home():
@@ -19,9 +27,23 @@ def login():
         print(job)
         print(questions)
 
+        response = openai.Completion.create(
+        engine="text-davinci-001",
+        prompt=f"Create a list of {questions} questions for my interview as a {job}\n",
+        temperature=0.5,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+)
+        text = response.choices[0].text
+        print(text)        
+
+
         return render_template("interview.html", 
-        job=job
+        text=text
         )
+
     else:
 	    return render_template("login.html")
 
